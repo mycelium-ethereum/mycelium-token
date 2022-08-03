6816af4accf0036faeb6d41376a6872dee02407d
Ray Mogg <raymondmogg@gmail.com>
2022-08-02 21:26:19 -0600

    E2E Testing (#6)
    * base repo cleanup. Rename files, move to yarn, import OZ via yarn
    
    * add basic testing. Integrate linter and CI
    
    * ci fix
    
    * fix migration functions. Add migration tests
    
    * add hardhat toolbox
    
    * finalise basic testing
    
    * lint
    
    * Update contracts/test/TestERC20.sol
    
    Co-authored-by: __CalabashSquash__ <my_name_uhh@protonmail.com>
    
    * Update contracts/TokenMigration.sol
    
    Co-authored-by: __CalabashSquash__ <my_name_uhh@protonmail.com>
    
    * Update contracts/TokenMigration.sol
    
    Co-authored-by: __CalabashSquash__ <my_name_uhh@protonmail.com>
    
    * update error messages. Remove modifier
    
    * patch tests
    
    * add e2e tests
    
    * lint
    
    Co-authored-by: __CalabashSquash__ <my_name_uhh@protonmail.com>

---
+++ test/e2e.js
@@ -0,0 +1,93 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("End to end migration test", function () {
+  let signers, TCR, MYC, migrationContract;
+  beforeEach(async () => {
+    signers = await ethers.getSigners();
+
+    const tokenFactory = await ethers.getContractFactory("TestToken");
+    const MYCTokenFactory = await ethers.getContractFactory("MYCToken");
+    TCR = await tokenFactory.deploy("Tracer", "TCR");
+    MYC = await MYCTokenFactory.deploy(signers[0].address, "Mycelium", "MYC");
+
+    const migrationFactory = await ethers.getContractFactory("TokenMigration");
+    migrationContract = await migrationFactory.deploy(
+      signers[0].address,
+      MYC.address,
+      TCR.address,
+      signers[1].address
+    );
+  });
+
+  describe("end to end", async () => {
+    beforeEach(async () => {
+      // mint some sample TCR out
+      for (var i = 3; i < 6; i++) {
+        // mint TCR to account 3
+        await TCR.mint(signers[i].address, ethers.utils.parseEther("100"));
+      }
+    });
+
+    it("reverts if migration contract is not a minter", async () => {
+      await TCR.connect(signers[3]).approve(
+        migrationContract.address,
+        ethers.utils.parseEther("100")
+      );
+      await expect(
+        migrationContract
+          .connect(signers[3])
+          .migrate(ethers.utils.parseEther("50"))
+      ).to.be.revertedWith("MYC:NOT_MINTER");
+    });
+
+    it("reverts if minting is paused on the token", async () => {
+      // set the migration contract as a minter
+      await MYC.grantRole(
+        ethers.utils.id("MINTER_ROLE"),
+        migrationContract.address
+      );
+
+      await MYC.setMintingPaused(true);
+
+      await TCR.connect(signers[3]).approve(
+        migrationContract.address,
+        ethers.utils.parseEther("100")
+      );
+      await expect(
+        migrationContract
+          .connect(signers[3])
+          .migrate(ethers.utils.parseEther("50"))
+      ).to.be.revertedWith("MYC:MINTING_PAUSED");
+    });
+
+    it("migrates tokens", async () => {
+      // set the migration contract as a minter
+      await MYC.grantRole(
+        ethers.utils.id("MINTER_ROLE"),
+        migrationContract.address
+      );
+
+      // perform migration
+      let MYCBalBefore = await MYC.balanceOf(signers[3].address);
+      let TCRBalBefore = await TCR.balanceOf(signers[3].address);
+      await TCR.connect(signers[3]).approve(
+        migrationContract.address,
+        ethers.utils.parseEther("100")
+      );
+      await migrationContract
+        .connect(signers[3])
+        .migrate(ethers.utils.parseEther("50"));
+      let MYCBalAfter = await MYC.balanceOf(signers[3].address);
+      let TCRBalAfter = await TCR.balanceOf(signers[3].address);
+      let diffBal = MYCBalAfter.sub(MYCBalBefore);
+      let diffTCRBal = TCRBalBefore.sub(TCRBalAfter);
+      expect(diffBal.toString()).to.equal(
+        ethers.utils.parseEther("50").toString()
+      );
+      expect(diffTCRBal.toString()).to.equal(
+        ethers.utils.parseEther("50").toString()
+      );
+    });
+  });
+});
