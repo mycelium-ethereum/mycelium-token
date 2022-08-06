c18f73013d9dce9ebe022b375861d1abf6022862
CalabashSquash <my_name_uhh@protonmail.com>
2022-08-06 13:41:46 +1000

    Improve natspec comments + add Migrated event emission

--- contracts/TokenMigration.sol
+++ contracts/TokenMigration.sol
@@ -10,10 +10,11 @@ interface IERC20Mintable {
 }
 
 /**
-* Migration contract supporting the transition from TCR to MYC.
-* Allows users to call the `migrate` function, exchanging TCR to MYC at a 1:1 ratio.
-* All burned TCR will be held in the contract.
-*/
+ * @title A TCR token to MYC token migration contract.
+ * @author raymogg
+ * @notice Allows users to call the `migrate` function, exchanging TCR for MYC at a 1:1 ratio.
+ * @dev All burned TCR will be held in the contract.
+ */
 contract TokenMigration is AccessControl {
     IERC20 public immutable myc;
     IERC20 public immutable tcr;
@@ -23,8 +24,21 @@ contract TokenMigration is AccessControl {
     // total amount of TCR successfully burned
     uint256 public burnedTCR;
 
-    event Migrated(address, uint256);
+    /**
+     * @notice Emits when some TCR tokens are migrated to MYC tokens.
+     * @param from The sender of the TCR tokens.
+     * @param to The recipient of the MYC tokens.
+     * @param amount The amount of TCR or MYC tokens.
+     * @dev `amount` will be the same for TCR and MYC tokens as migration is done at a 1:1 ratio.
+     */
+    event Migrated(address from, address to, uint256 amount);
 
+    /**
+     * @notice Sets up the `DEFAULT_ADMIN_ROLE` role and assigns values for the MYC and TCR tokens.
+     * @param admin The address to whom the `DEFAULT_ADMIN_ROLE` role will be assigned.
+     * @param _myc The MYC token address.
+     * @param _tcr The TCR token address.
+     */
     constructor(
         address admin,
         address _myc,
@@ -36,27 +50,32 @@ contract TokenMigration is AccessControl {
     }
 
     /**
-    * @notice allows an admin to set the migration NFT contract address
-    * @param _nft address to be used to mint the NFTs. Must support the 
-    * IMigrationNFT interface
-    */
-    function setNFTContract(address _nft) public {
+     * @notice Set the migration NFT contract address.
+     * @param _nft Address to be used to mint the NFTs.
+     * @custom:requirements The contract at address `_nft` must implement the IMigrationNFT interface.
+     * @custom:requirements `msg.sender` is a member of the `DEFAULT_ADMIN_ROLE` role.
+     */
+    function setNFTContract(address _nft) external {
         require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "NOT_ADMIN");
         nft = IMigrationNFT(_nft);
     }
 
     /**
-    * @notice allows an admin to enable and disable migration / minting
-    */
+     * @notice Enable or disable migration & minting.
+     * @param state True of minting is to be paused, false if not.
+     * @custom:requirements `msg.sender` is a member of the `DEFAULT_ADMIN_ROLE` role.
+     */
     function setMintingPaused(bool state) external {
         require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "NOT_ADMIN");
         mintingPaused = state;
     }
 
     /**
-    * @notice safety function to allow an admin to withdraw any tokens accidently sent to this contract.
-    * @dev does not check if tokens were sent by mistake or properly migrated.
-    */
+     * @notice Safety function to allow an admin to withdraw any tokens accidently sent to this contract.
+     * @param token The ERC20 token address to transfer out of this contract address.
+     * @dev Does not check if tokens were sent by mistake or properly migrated.
+     * @custom:requirements `msg.sender` is a member of the `DEFAULT_ADMIN_ROLE` role.
+     */
     function withdrawTokens(address token) external {
         require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "NOT_ADMIN");
         IERC20(token).transfer(
@@ -65,27 +84,39 @@ contract TokenMigration is AccessControl {
         );
     }
 
+    /**
+     * @notice Migrate `amount` TCR tokens to `amount` MYC tokens, and transfer to a specified address.
+     * @param amount The amount of TCR to be burnt, and thus MYC to be minted.
+     * @param to The recipient address of the minted MYC tokens.
+     */
     function migrateTo(uint256 amount, address to) external {
         require(to != address(0), "Migrating to 0 address");
         _migrate(amount, to, msg.sender);
     }
 
+    /**
+     * @notice Migrate `amount` TCR tokens to `amount` MYC tokens, and transfer to a the calling address.
+     * @param amount The amount of TCR to be burnt, and thus MYC to be minted.
+     */
     function migrate(uint256 amount) external {
         _migrate(amount, msg.sender, msg.sender);
     }
 
     /**
-    * @notice allows the exchange of TCR for MYC at a 1:1 ratio.
-    * holds migrated TCR in this contract and mints fresh MYC to the to address.
-    * @param amount the amount of TCR that is being burned
-    * @param to the receiver of MYC
-    * @param from the burner of TCR
-    */
+     * @notice Allows the exchange of TCR for MYC at a 1:1 ratio.
+     * @param amount The amount of TCR that is being migrated to MYC.
+     * @param to The recipient address of MYC tokens.
+     * @param from The address burning their TCR.
+     * @dev This contract holds migrated TCR and mints fresh MYC to the `to` address.
+     * @dev Emits a `Migrated` event.
+     * @custom:requirement `mintingPaused == false`.
+     * @custom:requirement `amount > 0`.
+     */
     function _migrate(
         uint256 amount,
         address to,
         address from
-    ) internal {
+    ) private {
         require(!mintingPaused, "MINTING_PAUSED");
         require(amount > 0, "INVALID_AMOUNT");
         // todo: add counter for amount of tokens "burned" via migration
@@ -93,11 +124,12 @@ contract TokenMigration is AccessControl {
         require(success, "XFER_ERROR");
         burnedTCR += amount;
         IERC20Mintable(address(myc)).mint(to, amount);
-        
+
         // issue NFT if this account has not yet migrated before
         if (!mintedNFT[to]) {
             mintedNFT[to] = true;
             nft.mintNFT(to);
         }
+        emit Migrated(from, to, amount);
     }
 }
