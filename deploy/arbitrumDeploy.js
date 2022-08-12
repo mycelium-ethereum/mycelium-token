48d3c3edb862e5d7d80901c7db61766d1b42676b
raymogg <raymondmogg@gmail.com>
2022-08-12 15:55:29 +1000

    change contract deployed

--- deploy/arbitrumDeploy.js
+++ deploy/arbitrumDeploy.js
@@ -5,7 +5,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   let arbMYC = "0xc74fe4c715510ec2f8c61d70d397b32043f55abe";
   let arbTCR = "0xA72159FC390f0E3C6D415e658264c7c4051E9b87";
 
-  let migrationContract = await deploy("TokenMigration", {
+  let migrationContract = await deploy("ArbitrumTokenMigration", {
     from: deployer,
     args: [deployer, arbMYC, arbTCR],
     log: true,
