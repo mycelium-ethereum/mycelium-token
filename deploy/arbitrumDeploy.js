29a98ede4a4cdbf040de99f689246b4387937a46
raymogg <raymondmogg@gmail.com>
2022-08-12 14:10:32 +1000

    add token addresses

--- deploy/arbitrumDeploy.js
+++ deploy/arbitrumDeploy.js
@@ -2,9 +2,8 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, execute } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  // todo: Migrate MYC and insert Arbitrum Token Address
-  let arbMYC = "";
-  let arbTCR = "";
+  let arbMYC = "0xc74fe4c715510ec2f8c61d70d397b32043f55abe";
+  let arbTCR = "0xA72159FC390f0E3C6D415e658264c7c4051E9b87";
 
   let migrationContract = await deploy("TokenMigration", {
     from: deployer,
